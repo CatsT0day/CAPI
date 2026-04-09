7e428d59b4ca681d465b81d3aded20a863815bb5
CatsT0day <ilya.n.ivlev@yandex.ru>
2026-04-09 17:31:24 +0300

    Initial commit

---
+++ src/main/java/me/catst0day/capi/CAPI.java
@@ -0,0 +1,17 @@
+package me.catst0day.capi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
